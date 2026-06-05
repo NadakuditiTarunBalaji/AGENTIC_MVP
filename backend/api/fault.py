79bcb31fecf9237126d64147519c2025b7316447
nadakuditi tarun balaji <nadakudititarunbalaji@gmail.com>
2026-06-05 11:41:20 +0530

    faults put delete added

--- backend/api/fault.py
+++ backend/api/fault.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import Session
 from backend.config.database import get_db
 from backend.services.fault_service import (
     fetch_faults,
-    create_fault
+    create_fault,
+    modify_fault,
+    remove_fault
 )
 
 router = APIRouter(
@@ -30,4 +32,30 @@ def add_fault(
         fault_name,
         severity,
         root_cause
+    )
+@router.put("/")
+@router.put("/{fault_id}")
+def update_fault_api(
+    fault_id: str,
+    fault_name: str,
+    root_cause: str,
+    severity: str,
+    db: Session = Depends(get_db)
+):
+    return modify_fault(
+        db,
+        fault_id,
+        fault_name,
+        root_cause,
+        severity
+    )
+
+@router.delete("/{fault_id}")
+def delete_fault_api(
+    fault_id: str,
+    db: Session = Depends(get_db)
+):
+    return remove_fault(
+        db,
+        fault_id
     )
