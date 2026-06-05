1f0a3d5953d465660e02b64af1bb945c9ee7a0a5
nadakuditi tarun balaji <nadakudititarunbalaji@gmail.com>
2026-06-05 12:21:15 +0530

    vehicle data added

--- backend/services/vehicle_service.py
+++ backend/services/vehicle_service.py
@@ -1,6 +1,8 @@
 from backend.repositories.vehicle_repository import (
     get_all_vehicle_data,
-    create_vehicle
+    create_vehicle,
+    update_vehicle_data,
+    delete_vehicle_data
 )
 
 def fetch_vehicle_data(db):
@@ -19,4 +21,30 @@ def add_vehicle(
         battery_temp,
         coolant_temp,
         speed
+    )
+
+def modify_vehicle_data(
+    db,
+    vehicle_id,
+    rpm,
+    battery_temp,
+    coolant_temp,
+    speed
+):
+    return update_vehicle_data(
+        db,
+        vehicle_id,
+        rpm,
+        battery_temp,
+        coolant_temp,
+        speed
+    )
+
+def remove_vehicle_data(
+    db,
+    vehicle_id
+):
+    return delete_vehicle_data(
+        db,
+        vehicle_id
     )
